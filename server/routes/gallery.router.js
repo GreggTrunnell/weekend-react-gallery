7498b1a8c10e9cdcfa047ab6cb4d0859b3e17b56
Gregg Trunnell <gggtrunnell@gmail.com>
2024-11-23 12:38:51 -0600

    wrote get route in router

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+//need to require the pool.js file
+const pool = require('../modules/pool.js');
 
 // PUT /gallery/like/:id
 router.put('/like/:id', (req, res) => {
@@ -7,8 +9,20 @@ router.put('/like/:id', (req, res) => {
 });
 
 // GET /gallery
+
 router.get('/', (req, res) => {
   // code here
+  console.log('router.get',response.data);
+  const queryText=`SELECT * FROM gallery`
+  pool.query(queryText)
+    .then((result)=>{
+    //?might need to change what the response data sends. ie change results.id
+      res.send(results.id)
+      .catch((error)=>{
+        console.log('error in router.get', error);
+        res.sendStatus(500)
+      })
+    })
 });
 
 module.exports = router;
